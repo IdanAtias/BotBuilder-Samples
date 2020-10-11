5dddb34123cbad7fddde867fb5d62f8860bde973
Idan Atias <idan.atias@kaminario.com>
2020-10-11 23:50:42 +0300

    use Silk tenant Id
    Signed-off-by: Idan Atias <idan.atias@kaminario.com>

--- samples/python/02.echo-bot/app.py
+++ samples/python/02.echo-bot/app.py
@@ -23,7 +23,7 @@ CONFIG = DefaultConfig()
 
 # Create adapter.
 # See https://aka.ms/about-bot-adapter to learn more about how bots work.
-SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
+SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD, CONFIG.APP_AUTH_TENANT)
 ADAPTER = BotFrameworkAdapter(SETTINGS)
 
 
